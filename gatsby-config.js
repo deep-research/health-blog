b537b1ba1240832d5f319f3f97aae0fa6df04e10
Victor <deep-research>
2018-08-16 14:14:20 -0500

    React Helmet Customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'Health Writings Blog',
+    description: 'healthwritings.com: A blog with practical ideas for improving your health!',
+    siteUrl: 'http://healthwritings.com/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
